e4dcce5a4638883638c85ab1776a0e58c524d0c4
Yukihiro Okada <yuokada@yahoo-corp.jp>
2018-07-06 15:52:38 +0900

    Reformat code

--- src/main/java/jp/co/yahoo/dataplatform/config/Configuration.java
+++ src/main/java/jp/co/yahoo/dataplatform/config/Configuration.java
@@ -15,119 +15,119 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package jp.co.yahoo.dataplatform.config;
 
 import java.io.IOException;
-
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import java.util.Map;
-import java.util.HashMap;
 
-import java.io.Serializable;
+public class Configuration implements Serializable {
 
-public class Configuration implements Serializable{
+  private final Map<String, String> settingContainer;
 
-  private final Map<String,String> settingContainer;
-
-  public Configuration(){
+  public Configuration() {
     settingContainer = new HashMap<>();
   }
 
-  public void set( final String key , final String value ){
-    settingContainer.put( key , value );
+  public void set(final String key, final String value) {
+    settingContainer.put(key, value);
   }
 
-  public void add(final Properties properties){
-    properties.entrySet().forEach(property -> set(property.getKey().toString(), property.getValue().toString()));
+  public void add(final Properties properties) {
+    properties
+        .entrySet()
+        .forEach(property -> set(property.getKey().toString(), property.getValue().toString()));
   }
 
-  public void add(final Iterable<Map.Entry<String,String>> settingEntries){
+  public void add(final Iterable<Map.Entry<String, String>> settingEntries) {
     settingEntries.forEach(setting -> set(setting.getKey(), setting.getValue()));
   }
 
-  public void add(final Configuration configuration){
+  public void add(final Configuration configuration) {
     settingContainer.putAll(configuration.toMap());
   }
 
-  public void clear(){
+  public void clear() {
     settingContainer.clear();
   }
 
-  public Map<String,String> toMap(){
-    Map<String,String> retVal = new HashMap<String,String>();
-    for( Map.Entry<String,String> entry : settingContainer.entrySet() ){
-      retVal.put( new String( entry.getKey() ) , new String( entry.getValue() ) );
+  public Map<String, String> toMap() {
+    Map<String, String> retVal = new HashMap<String, String>();
+    for (Map.Entry<String, String> entry : settingContainer.entrySet()) {
+      retVal.put(new String(entry.getKey()), new String(entry.getValue()));
     }
     return retVal;
   }
 
-  public Set<String> getKey(){
+  public Set<String> getKey() {
     return settingContainer.keySet();
   }
 
-  public String get( final String key ){
-    return get( key , null );
+  public String get(final String key) {
+    return get(key, null);
   }
 
-  public String get( final String key , final String defaultValue ){
-    String retVal = settingContainer.get( key );
-    if( retVal == null ){
+  public String get(final String key, final String defaultValue) {
+    String retVal = settingContainer.get(key);
+    if (retVal == null) {
       return defaultValue;
     }
     return retVal;
   }
 
-  public int getInt( final String key ){
-    return Integer.parseInt( settingContainer.get( key ) );
+  public int getInt(final String key) {
+    return Integer.parseInt(settingContainer.get(key));
   }
 
-  public int getInt( final String key , final int defaultValue ){
-    String target = settingContainer.get( key );
-    if( target == null ){
+  public int getInt(final String key, final int defaultValue) {
+    String target = settingContainer.get(key);
+    if (target == null) {
       return defaultValue;
     }
-    return Integer.parseInt( target );
+    return Integer.parseInt(target);
   }
 
-  public long getLong( final String key ){
-    return Long.parseLong( settingContainer.get( key ) );
+  public long getLong(final String key) {
+    return Long.parseLong(settingContainer.get(key));
   }
 
-  public long getLong( final String key , final long defaultValue ){
-    String target = settingContainer.get( key );
-    if( target == null ){
+  public long getLong(final String key, final long defaultValue) {
+    String target = settingContainer.get(key);
+    if (target == null) {
       return defaultValue;
     }
-    return Long.parseLong( target );
+    return Long.parseLong(target);
   }
 
-  public double getDouble( final String key ){
-    return Double.parseDouble( settingContainer.get( key ) );
+  public double getDouble(final String key) {
+    return Double.parseDouble(settingContainer.get(key));
   }
 
-  public double getDouble( final String key , final double defaultValue ){
-    String target = settingContainer.get( key );
-    if( target == null ){
+  public double getDouble(final String key, final double defaultValue) {
+    String target = settingContainer.get(key);
+    if (target == null) {
       return defaultValue;
     }
-    return Double.parseDouble( target );
+    return Double.parseDouble(target);
   }
 
-  public boolean containsKey( final String key ) throws IOException{
-    return settingContainer.containsKey( key );
+  public boolean containsKey(final String key) throws IOException {
+    return settingContainer.containsKey(key);
   }
 
-  public Object getObject( final String key ) throws IOException{
-    return getObject( key , settingContainer.get( key ) );
+  public Object getObject(final String key) throws IOException {
+    return getObject(key, settingContainer.get(key));
   }
 
-  public Object getObject( final String key , final String defaultValue ) throws IOException{
-    String targetClassName = settingContainer.get( key );
-    if( targetClassName == null || targetClassName.isEmpty() ){
+  public Object getObject(final String key, final String defaultValue) throws IOException {
+    String targetClassName = settingContainer.get(key);
+    if (targetClassName == null || targetClassName.isEmpty()) {
       targetClassName = defaultValue;
     }
-    return FindClass.getObject( targetClassName );
+    return FindClass.getObject(targetClassName);
   }
-
 }
